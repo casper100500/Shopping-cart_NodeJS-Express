820f12ff1c5864a4e6d4a5e5078942133c057f94
Casper100500 <coolbox100500@yahoo.com>
2022-07-13 12:33:51 +0300

    EX2 bad version

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressHbs= require ('express-handlebars')
+var index = require('./routes/index');
 
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs',expressHbs({defaultLayout: 'layout',extname: '.hbs' }))
+app.set('view engine', '.hbs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', index);
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
