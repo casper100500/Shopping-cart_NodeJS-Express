45d8158b28d24165198c97165dcc920390a67481
Casper100500 <coolbox100500@yahoo.com>
2022-07-13 12:36:56 +0300

    EX3-4 Seeding Data_mongodb_docker...

---
+++ models/product.js
@@ -0,0 +1,14 @@
+console.log(`Creating products table`);
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/shopping')
+var Schema = mongoose.Schema;
+var schema = new Schema({
+    imagePath: {type: String, required: true},
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+});
+console.log(`almost done`);
+module.exports = mongoose.model('Product', schema)
+console.log(`Done`);
+mongoose.disconnect;
