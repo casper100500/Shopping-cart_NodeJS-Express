03b0a71a395424f481332a474fec5c346e970863
Casper100500 <coolbox100500@yahoo.com>
2022-07-13 12:51:23 +0300

    EX16_MakingChargeswithStripe_by_NG_OK_v2

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -1,58 +1,127 @@
 //Created by NG. Study lesson completly different
 //
 //my pk
+//var stripe = Stripe('pk_test_51LGe7YGUvmt8rPeQGIdTgAWEjHZkvRtZmggh90wpzWqLy0XKW0fNv7xDuwS2KOFdGn9zjR0JsdKbAg6G0P2yevqR00wCqpiygk');
+// CardNumber example 374245455400126
+// CardNumber example 4242424242424242
+// The items the customer wants to buy
+// A reference to Stripe.js initialized with a fake API key.
+// Sign in to see examples pre-filled with your key.
 var stripe = Stripe('pk_test_51LGe7YGUvmt8rPeQGIdTgAWEjHZkvRtZmggh90wpzWqLy0XKW0fNv7xDuwS2KOFdGn9zjR0JsdKbAg6G0P2yevqR00wCqpiygk');
 
-var elements = stripe.elements();
-var style = {
-  base: {
-    color: "#32325d",
-    fontFamily: 'Arial, sans-serif',
-    fontSmoothing: "antialiased",
-    fontSize: "16px",
-    "::placeholder": {
-      color: "#32325d"
-    }
-  },
-  invalid: {
-    fontFamily: 'Arial, sans-serif',
-    color: "#fa755a",
-    iconColor: "#fa755a"
-  }
+console.log('here...')
+
+// The items the customer wants to buy
+var purchase = {
+  items: [{ id: "xl-tshirt" }]
 };
-console.log('Card here...')
-var card = elements.create("card", { style: style });
-// Stripe injects an iframe into the DOM
-card.mount("#card-element");
-  //  var elements = stripe.elements();
-  var $form = $('#checkout-form');
 
-  $form.submit(function(event){
-          $form.find('button').prop('disabled',true); //Disable submission
-          var cardElement=
-          {"cardnumber":"374245455400126",
-          "exp-date":"12/2033",
-          "cvc":"100",
-          "postal":"4324324"
-          }
-          var cardElement2=
-          {number:$('#card-number').val(),
-          cvc:$('#card-cvc').val(),
-          exp_month:$('#card-expiry-month').val(),
-          exp_year:$('#card-expiry-year').val()
-          }
-      console.log(cardElement)
-      console.log(cardElement2)
-      // const elements = stripe.elements();
-      // const card = elements.create('card');
-     
-     // card.mount(cardElement);
-    
-        // stripe.createToken(cardElement).then(function(result) {
-        //   console.log(result)
-        //   });
+// Disable the button until we have Stripe set up on the page
+document.querySelector("button").disabled = true;
+fetch("/create-payment-intent", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(purchase)
+})
+  .then(function(result) {
+    return result.json();
+  })
+  .then(function(data) {
+    var elements = stripe.elements();
+    var style = {
+      base: {
+        color: "#32325d",
+        fontFamily: 'Arial, sans-serif',
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#32325d"
+        }
+      },
+      invalid: {
+        fontFamily: 'Arial, sans-serif',
+        color: "#fa755a",
+        iconColor: "#fa755a"
+      }
+    };
+    console.log('Card here...')
+    var card = elements.create("card", { style: style });
+    // Stripe injects an iframe into the DOM
+    card.mount("#card-element");
 
-        return false;
+    card.on("change", function (event) {
+      // Disable the Pay button if there are no card details in the Element
+      document.querySelector("button").disabled = event.empty;
+      document.querySelector("#card-error").textContent = event.error ? event.error.message : "";
+    });
+
+    var form = document.getElementById("payment-form");
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      // Complete payment when the submit button is clicked
+      payWithCard(stripe, card, data.clientSecret);
+    });
   });
 
+// Calls stripe.confirmCardPayment
+// If the card requires authentication Stripe shows a pop-up modal to
+// prompt the user to enter authentication details without leaving your page.
+var payWithCard = function(stripe, card, clientSecret) {
+  loading(true);
+  stripe
+    .confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card
+      }
+    })
+    .then(function(result) {
+      if (result.error) {
+        // Show error to your customer
+        showError(result.error.message);
+      } else {
+        // The payment succeeded!
+        orderComplete(result.paymentIntent.id);
+      }
+    });
+};
 
+/* ------- UI helpers ------- */
+
+// Shows a success message when the payment is complete
+var orderComplete = function(paymentIntentId) {
+  loading(false);
+  document
+    .querySelector(".result-message a")
+    .setAttribute(
+      "href",
+      "https://dashboard.stripe.com/test/payments/" + paymentIntentId
+    );
+  document.querySelector(".result-message").classList.remove("hidden");
+  document.querySelector("button").disabled = true;
+};
+
+// Show the customer the error from Stripe if their card fails to charge
+var showError = function(errorMsgText) {
+  loading(false);
+  var errorMsg = document.querySelector("#card-error");
+  errorMsg.textContent = errorMsgText;
+  setTimeout(function() {
+    errorMsg.textContent = "";
+  }, 4000);
+};
+
+// Show a spinner on payment submission
+var loading = function(isLoading) {
+  if (isLoading) {
+    // Disable the button and show a spinner
+    document.querySelector("button").disabled = true;
+    document.querySelector("#spinner").classList.remove("hidden");
+    document.querySelector("#button-text").classList.add("hidden");
+  } else {
+    document.querySelector("button").disabled = false;
+    document.querySelector("#spinner").classList.add("hidden");
+    document.querySelector("#button-text").classList.remove("hidden");
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var express = require('express');
 var router = express.Router();
 var Product = require('../models/product')
 var Cart = require('../models/cart')
+//var Stripe=require('stripe') //Payment wesite
+const stripe = require("stripe")("sk_test_51LGe7YGUvmt8rPeQ8q2gvYL4SIP25obw5Oj7IwVkqjs00xWDBOvfIyCh10kur6gMgRMV2rV4DgjGceWL4SXLKLuD00yCXXOQ1P");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 var Products=Product.find(function(err,docs){
@@ -18,7 +21,7 @@ for (var i = 0; i< docs.length;i +=chunkSize)
   //slice=mid function in VBA
 }
 
-res.render('shop/index', { title: 'Shopping Cart_EX15 Stripe Verifying Credit Cards', products:productChunks });
+res.render('shop/index', { title: 'Shopping Cart_EX16 Making Charges with Stripe', products:productChunks });
   
   }).lean(); //patch by NG added .lean();!!!!
  
@@ -64,12 +67,71 @@ res.render('shop/shopping-cart', { products:cart.generateArray(), totalPrice:car
 
 
 router.get('/checkout', function(req, res, next) {
-  //if(!req.session.cart)  {    return res.redirect('/shopping-cart');  }
- // var cart = new Cart(req.session.cart)
-//  res.render('shop/checkout', { total:cart.totalPrice});
-  res.render('shop/checkout');
+  if(!req.session.cart)  {    return res.redirect('shop/shopping-cart');  }
+
+  var cart = new Cart(req.session.cart)
+  res.render('shop/checkout', { total:cart.totalPrice});
+//  res.render('shop/checkout');
 
 })
+
+const calculateOrderAmount = items => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  console.log(items.totalPrice)
+  if (items.totalPrice>0)
+  {//console.log('my Stripe - totalPrice!!!!')
+    return items.totalPrice*100;
+  }
+  else
+  {
+  return 100;
+  }
+};
+
+router.post("/create-payment-intent", async (req, res) => {
+  //var cart = new Cart(req.session.cart)
+ // res.render('shop/shopping-cart', { products:cart.generateArray(), totalPrice:cart.totalPrice});
+
+  const { items } = req.body;
+  console.log('my Stripe - Success!!!!')
+  //console.log(req.body)
+  //console.log(cart.generateArray())
+  console.log(req.session.cart)
+ // if (!req.session.cart)
+ //  var cart = new Cart(req.session.cart)
+     var cart= new Cart(req.session.cart ? req.session.cart :{});
+
+    arr=cart.generateArray();
+    //console.log('****************')
+    console.log(arr)
+    //console.log('****************')
+    //console.log(typeof arr)
+
+    titles=''
+    productIds=''
+    arr.forEach(element =>  titles+=element.item.title+', ' );
+  //  arr.forEach(element =>  productIds+=element.item.title+' - '+element.item._id+'\n' );
+      
+    titles=titles.slice(0,-2)
+   // productIds=productIds.slice(0,-2)
+    description=titles+'\n'+productIds
+    console.log(description)
+  
+
+  // Create a PaymentIntent with the order amount and currency
+   const paymentIntent = await stripe.paymentIntents.create(
+     { description:description,
+       amount: calculateOrderAmount(cart), 
+       currency: "eur"  });
+
+    //  const paymentIntent = await stripe.paymentIntents.create( { amount: calculateOrderAmount(items),  currency: "usd"  });
+
+
+  res.send({clientSecret: paymentIntent.client_secret});
+
+});
 
 module.exports = router;
 
